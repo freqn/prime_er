e67e813aef2803cab743e306e07dacd0b566b5f7
Chip Eyler <ceyler@gmail.com>
2014-01-18 12:09:16 -0500

    Updated filename

--- prim_er.rb
+++ prim_er.rb
@@ -2,8 +2,6 @@ require 'prime'
 
 def first_n_primes(n)
   # Check for correct input!
-  "n must be an integer" unless n.is_a? Integer
-  "n must be greater than 0" if n <= 0
 
   # The Ruby 1.9 Prime class makes the array automatically!
   prime = Prime.instance
@@ -16,4 +14,4 @@ puts "How many primes would you like to generate?"
 puts "Please enter a number: "
 result = first_n_primes(gets.chomp.to_i)
 num = 0
-display = result.each { |x| puts "Prime #{num += 1} = #{x}"}
+display = result.each { |x| puts "Prime ##{num += 1} = #{x}"}
